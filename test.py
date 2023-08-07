ce443279611d91f1c9aca50fe6c1bb40a531be6b
Harold JC <69402668+imharoldjc@users.noreply.github.com>
2023-08-07 08:43:00 -0400

    thinking

---
+++ test.py
@@ -0,0 +1,2 @@
+for i in range(3):
+	print('hello'+str(i)) #0,1,2
--- wordlePy.py
+++ wordlePy.py
@@ -12,21 +12,25 @@ f.close()
 
 
 playAgain = True
-while playAgain:
+while playAgain:  # start game loop
     print("*** W O R D L E ***")
 
     tries = 0
-    validation = ''
-    solution = random.choice(answerbank)
+    feedback = ''
+    solution = random.choice(answerbank) # this game's word
 
-    while tries < 6:
+    while tries < 6:  # <= 5 tries
 
-        tries += 1
-        attempt = ""
+        tries += 1  # current try number
+        attempt = "" # player's input
 
-        # Check if attempt is valid.
+        # Check if attempt is valid. Prompt for attempt until a valid attempt entered.
         while len(attempt) != 5 or attempt.isalpha() != True or attempt not in guessbank:
             attempt = (input("Attempt " + str(tries) + "/6: ")).lower()
+            if len(attempt) != 5:
+                print('Must enter a five-letter word.')
+            if attempt.isalpha() != True:
+                print('All characters must be in the alphabet. (a-z, A-Z)')
             if attempt not in guessbank:
                 print('Not in word list.\n')
 
@@ -34,8 +38,10 @@ while playAgain:
             print("CORRECT!\n")
             break
 
-        validation = list(attempt)  # Feedback output
-        solutionArr = list(solution) # Copy of solution in list form
+        # Generate feedback output # could use slices? or smth instead of making a list #hashmap?
+        feedback = list(attempt)  # Feedback output
+        #print(attempt) attempt wasnt turned into a list
+        solutionArr = list(solution) # Copy of solution in list form for comparison to attempt
 
         # Check for correct characters
         for i in range(len(attempt)):
@@ -43,18 +49,18 @@ while playAgain:
                 solutionArr[i] = 'found'
 
         # Generate attempt feedback
-        for i in range(len(attempt)):
+        for i in range(len(attempt)):  # for each letter
             letterCount = solutionArr.count(attempt[i])
 
             # Letter in answer but in wrong place
-            if letterCount > 0 and validation.count("*" + attempt[i] + "*") < letterCount and solutionArr[i] != 'found':
-                validation[i] = "*" + attempt[i] + "*"
+            if letterCount > 0 and feedback.count("*" + attempt[i] + "*") < letterCount and solutionArr[i] != 'found':
+                feedback[i] = "*" + attempt[i] + "*"
 
             # Letter not in answer at all or too many of that letter
             elif solutionArr[i] != 'found':
-                validation[i] = "-" + attempt[i] + "-"
+                feedback[i] = "-" + attempt[i] + "-"
 
-        print(("  ".join(validation)).upper(), "\n")
+        print(("  ".join(feedback)).upper(), "\n")
 
 
     if attempt != solution:
